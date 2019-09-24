3de5f67834c793450668cb42138ff3ef4fb29c53
James Cat <james@catplusplus.org.uk>
2019-09-24 16:54:42 +0100

    checking in router.js

--- src/router/Router.js
+++ src/router/Router.js
@@ -14,7 +14,7 @@ export default class Router {
     let regexPath = route.path.replace(/([:*])(\w+)/g, (full, colon, name) => {
       paramNames.push(name);
       return '([^\/]+)';
-    }) + '(?:\/|$)';
+    }) + '(?:\/|$)'
 
     let params = {};
     // match the requested path with the regex
@@ -38,10 +38,9 @@ export default class Router {
     const route = this.routes.filter(route => this.match(route, path))[0];
     if(!route) this.renderNode.innerHTML = '404! Page not found';
     else {
+      console.log(path);
       window.location.href = path.search('/#') === -1 ? '#' + path : path;
-      this.renderNode.innerHTML = route.renderView(); // innerHTML avoided
+      this.renderNode.innerHTML = route.renderView(); // innerHTML must be avoided
     }
   }
-
-
 }
