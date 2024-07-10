99987fc1b9520e070d06736615b6829ad65f7c3f
zz-james <zz.james@gmail.com>
2024-07-10 17:17:10 +0100

    initial commit

--- dbrestore.sh
+++ dbrestore.sh
@@ -3,7 +3,7 @@
 set -e
 
 # the name or id of the database container for this composition
-DB_CONTAINER=_database_container_name_or_id_
+DB_CONTAINER=ab358f72e8613c05bde67f269e5e3d31c0aec016b9e4055688492756eb6ec3a2
 
 # one way to get the above automatically
 # this only works if there is only one running container with 'database' in it's name
